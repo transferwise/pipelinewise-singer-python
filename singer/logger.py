615a64e17fb029675abaa0e48868cfa9d1541d79
Samira El Aabidi <samira.elaabidi@transferwise.com>
2020-02-03 11:55:07 +0200

    use LOGGING_CONF_FILE as a way to pass logging config

--- singer/logger.py
+++ singer/logger.py
@@ -1,7 +1,12 @@
 import logging
 import logging.config
+import os
 
 
 def get_logger(name='singer'):
     """Return a Logger instance to use in singer."""
+    if 'LOGGING_CONF_FILE' in os.environ and os.environ['LOGGING_CONF_FILE']:
+        path = os.environ['LOGGING_CONF_FILE']
+        logging.config.fileConfig(path, disable_existing_loggers=False)
+
     return logging.getLogger(name)
