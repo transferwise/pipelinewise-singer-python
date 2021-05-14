e276848ce486ae3fe6c2b3da62e6035c4e1d79f6
AJ Steers <aaronsteers@gmail.com>
2021-05-14 15:16:31 -0700

    zip_safe=False

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name="pipelinewise-singer-python",
           'backoff==1.10.0',
           'ciso8601',
       ],
+      zip_safe=False,
       extras_require={
           'dev': [
               'pylint',
