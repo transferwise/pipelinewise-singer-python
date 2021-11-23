24bbeee261e179062d78013fb659106b26953a44
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2021-11-23 14:49:16 +0000

    v2.0.1 (#46)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="pipelinewise-singer-python",
-      version='2.0.0',
+      version='2.0.1',
       description="Singer.io utility library - PipelineWise compatible",
       long_description=long_description,
       long_description_content_type="text/markdown",
