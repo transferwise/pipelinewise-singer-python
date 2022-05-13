a916726f1df113dd7511b2c9f09b4fa8535abbcc
Edgar Ramírez <edgarrm358@gmail.com>
2022-05-13 04:09:59 -0500

    Drop support for Python 3.6 (#64)

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open("README.md", "r") as fh:
 setup(name="pipelinewise-singer-python",
       version='2.0.1',
       description="Singer.io utility library - PipelineWise compatible",
+      python_requires=">=3.7.0, <3.11",
       long_description=long_description,
       long_description_content_type="text/markdown",
       author="TransferWise",
@@ -19,7 +20,7 @@ setup(name="pipelinewise-singer-python",
       install_requires=[
           'pytz',
           'jsonschema==3.2.0',
-          'orjson==3.6.1',
+          'orjson==3.6.5',
           'python-dateutil>=2.6.0',
           'backoff==1.11.1',
           'ciso8601',
@@ -27,9 +28,10 @@ setup(name="pipelinewise-singer-python",
       extras_require={
           'dev': [
               'pylint==2.11.1',
+              'pytest==7.1.2',
+              'coverage[toml]~=6.3',
               'ipython',
               'ipdb',
-              'nose',
               'unify==0.5'
           ]
       },
