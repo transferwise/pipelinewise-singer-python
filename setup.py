74fb6d1efc983883dac81ee64028716d1a750acc
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-05-12 21:15:04 -0500

    pinned coverage to version 6.*

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name="pipelinewise-singer-python",
           'dev': [
               'pylint==2.11.1',
               'pytest==7.1.2',
-              'coverage[toml]',
+              'coverage[toml]~=6.3',
               'ipython',
               'ipdb',
               'unify==0.5'
