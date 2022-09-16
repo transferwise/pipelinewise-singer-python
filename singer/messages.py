f569987f759bbf9f930deeab3f32b45df2c2db6f
mjsqu <mj2.squ@gmail.com>
2022-09-16 13:55:33 +1200

    Try removing raise TypeError

--- singer/messages.py
+++ singer/messages.py
@@ -294,8 +294,6 @@ def parse_message(msg):
 def orjson_default(obj):
     if isinstance(obj, decimal.Decimal):
         return str(obj)
-    else:
-        raise TypeError
 
 def format_message(message, option=0):
     return orjson.dumps(message.asdict(), option=option, default=orjson_default)
